530dd4e6a854b638c1c2bbd0949aa419f90a808d
SergeyBorodaenko <sergey.borodaenko@invent.us>
2022-01-23 00:11:51 +0300

    Init project

---
+++ WpTools/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WpTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var link = "https://www.space.com/20930-dark-matter.html";
+            var name = "20930-dark-matter.html";
+
+            var downloader = new SpaceComDownloader();
+            downloader.Run(link, name);
+
+            Console.WriteLine("Done");
+        }
+    }
+}
---
+++ WpTools/SpaceComDownloader.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using RestSharp;
+
+namespace WpTools
+{
+    public class SpaceComDownloader
+    {
+        public void Run(string link, string name)
+        {
+            // Download
+            var client = new RestClient(link);
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+
+            var s = response.Content;
+
+            // Save
+
+            var enviroment = System.Environment.CurrentDirectory;
+
+            var path = Directory.GetParent(enviroment)!.Parent!.Parent!.FullName;
+            
+            path = Path.Combine(path, "lake");
+
+            if (Directory.Exists(path) == false)
+                Directory.CreateDirectory(path);
+
+            path = Path.Combine(path, "spaceCom");
+
+            if (Directory.Exists(path) == false)
+                Directory.CreateDirectory(path);
+
+            path = Path.Combine(path, name);
+
+            File.WriteAllText(path, s);
+        }
+    }
+}
